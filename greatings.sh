eab3af255eb8c61135ccabfd73b0c57c5af033fe
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-09 09:46:24 +0530

    Add basic greeting shell script

---
+++ greatings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This is a basic shell script example
+
+# Print a greeting
+echo "Hello! Welcome to my script."
+
+# Ask the user for their name
+read -p "Please enter your name: " name
+
+# Print a personalized message
+echo "Nice to meet you, $name!"
+
+# Show current date and time
+echo "Current date and time: $(date)"
+
+# End of script
+echo "Script execution completed."
